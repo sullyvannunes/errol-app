8b417b96102ffd6ed57de11b0d00282bb837a303
sullyvan nunes <sullyvan.nunes@yahoo.com.br>
2018-04-18 01:49:37 -0300

    changes to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180418032029) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "messages", force: :cascade do |t|
     t.string "title"
     t.string "subject"
